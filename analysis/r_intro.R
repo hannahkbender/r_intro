981c1d7d3a905965ce4d08357c31791e279912ce
hannahkbender <hannah.k.bender@gmail.com>
2025-10-22 17:23:46 -0400

    So let it begin, the start of the intro process. https://imgflip.com/i/a9ww5q

---
+++ analysis/r_intro.R
@@ -0,0 +1,69 @@
+# Description
+
+# Author: Hannah
+# Version: 2025-10-31
+
+# Clear environment
+rm(list = ls())
+
+# Clean console
+cat("\014")
+
+# Packages
+library(janitor)
+library(glue)
+library(tidyverse)
+
+# Set directories
+project <- "~/econ_phd/code/projects/r_intro/data"
+import <- glue("{project}/import")
+build <- glue("{project}/build")
+export <- glue("{project}/export")
+
+# =======================================================
+# Intro to R Practice Program: Iris Dataset
+# =======================================================
+
+# 1. Basic exploration
+head(iris)          # first few rows
+summary(iris)       # summary statistics
+
+# 2. Mild cleaning and new var
+iris_clean <- iris %>% 
+  clean_names() %>% 
+  mutate(sepal_ratio = sepal_length/sepal_width)
+
+# 3. Descriptive statistics 
+summary_stats <- iris_clean %>%
+  group_by(species) %>%
+  summarize(
+    mean_length = mean(sepal_length),
+    mean_width  = mean(sepal_width),
+    sd_length   = sd(sepal_length),
+    sd_width    = sd(sepal_length),
+    count = n()
+    )
+
+# 4. Plot with ggplot2
+plot <- ggplot(iris_clean, aes(x = sepal_length, y = petal_length, color = species)) +
+  geom_point(size = 2) +
+  labs(
+    title = "Iris: Sepal vs Petal Length",
+    x = "Sepal Length (cm)",
+    y = "Petal Length (cm)"
+  ) +
+  theme_minimal()
+
+plot
+
+# 5. Save processed data
+write.csv(iris_clean, file.path(build, "iris_with_ratio.csv"), row.names = FALSE)
+
+# 6. Save summary stats and plot
+write_excel_csv(summary_stats, file.path(export, "summary_stats.xlsx")) # object then path
+ggsave(file.path(export, "sepal_v_petal_length_plot.png"), plot)        # path then object
+
+
+iris_clean %>% 
+  mutate(row_min = pmin(petal_width, petal_length))
+
---
+++ analysis/r_template.R
@@ -0,0 +1,31 @@
+# Description
+
+# Author: Name
+# Version: Date
+
+# Clear environment
+rm(list = ls())
+
+# Clean console
+cat("\014")
+
+# Packages
+library(janitor)
+library(glue)
+library(tidyverse)
+
+# Set directories
+project <- "~/projects/[PROJECT NAME]"
+import <- glue("{project}/data/import")
+build <- glue("{project}/data/build")
+export <- glue("{project}/export")
+
+# Parameters
+
+#===============================================================================
+
+# Code
+
+#===============================================================================
+
+
